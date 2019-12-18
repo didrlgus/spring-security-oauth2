72325ea5206749db4ef724d0295b8c53461efbe3
양기현 <rlgusdid@naver.com>
2019-12-18 13:45:54 +0900

    첫번째 커밋

---
+++ src/test/java/com/oauth2/example/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
